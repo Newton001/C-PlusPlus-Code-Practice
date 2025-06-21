5e73a4be5c6f177e5c089e8ca1aeb696018f5ffd
Newton Kelvin Ollengo <Newtonkelvin75@gmail.com>
2025-06-21 15:48:15 +0200

    Simple Calculator

--- Day_Nine/DayNine/main.cpp
+++ Day_Nine/DayNine/main.cpp
@@ -21,9 +21,47 @@ void array_reverse(vector<int> &array)
     }
 }
 
+float calculator()
+{
+    int num1, num2;
+
+    char op;
+
+    std::cout<<" Enter the first Number: ";
+    std::cin>>num1;
+
+    std::cout<<" Enter the second Number: ";
+    std::cin>>num2;
+
+    std::cout<< "Enter the operation ( +, -, *, /): ";
+    std::cin>>op;
+
+    float result;
+
+    if(op =='+'){
+        result = num1 + num2;
+    } else if(op == '-'){
+        result = num1 - num2;
+    } else if(op == '*'){
+        result = num1 * num2;
+    } else if(op == '/'){
+        if(num2 !=0)
+            result = static_cast<float>(num1/num2);
+        else{
+            std::cout<<"Error! Cannot divide by Zero"<<std::endl;
+            return -1;
+        }
+    }else{
+        std::cout<<" Invalid Operator" <<std::endl;
+        return -1;
+    }
+
+    return result;
+}
+
 int factorial(int n)
 {
-    if (n < 2) return n;
+    if (n <=1) return 1;
     return n * factorial(n-1);
 }
 
@@ -41,6 +79,8 @@ int main()
     std::cout<<" "<<std::endl;
 
     std::cout<<factorial(5)<< std::endl;
+
+    std::cout<<calculator()<<std::endl;
 
     return 0;
 }
